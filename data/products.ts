af6da9ae47f11910f0507c8c23f182cedeea138c
artbereh <a.berehovyi@elsevier.com>
2026-09-18 16:38:18 +0200

    refactor to parameterize products

---
+++ data/products.ts
@@ -0,0 +1,7 @@
+export const PRODUCTS = {
+    backpack: 'Sauce Labs Backpack',
+    light: 'Sauce Labs Bike Light',
+    tshirt: 'Sauce Labs Bolt T-Shirt',
+    jacket: 'Sauce Labs Fleece Jacket',
+    onesie: 'Sauce Labs Onesie',
+}
--- tests/UI/product.spec.ts
+++ tests/UI/product.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/loginPage';
 import { ProductsPage } from '../../pages/productsPage';
 import { CartPage } from '../../pages/cartPage';
+import { PRODUCTS } from '../../data/products';
 import { WEBSITE, STANDARD_USER } from '../../data/users';
 
 test('should add products to cart', async ({ page }) => {
@@ -12,11 +13,33 @@ test('should add products to cart', async ({ page }) => {
     await loginPage.open(WEBSITE.prod);
     await loginPage.login(STANDARD_USER.username, STANDARD_USER.password)
 
-    await productsPage.addItem('Sauce Labs Backpack');
-    await productsPage.addItem('Sauce Labs Bolt T-Shirt');
+    await productsPage.addItem(PRODUCTS.backpack);
+    await productsPage.addItem(PRODUCTS.tshirt);
     await expect (productsPage.shoppingCount).toHaveText('2');
     await productsPage.openShoppingCart();
 
     await expect(cartPage.backpack).toBeVisible(); 
     await expect(cartPage.tshirt).toBeVisible();
 })
+
+const products = [
+    PRODUCTS.backpack,
+    PRODUCTS.jacket,
+    PRODUCTS.light,
+    PRODUCTS.onesie,
+    PRODUCTS.tshirt,
+]
+
+for (const product of products) {
+  test(`should add ${product} to cart`, async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const productsPage = new ProductsPage(page);
+
+    await loginPage.open(WEBSITE.prod);
+    await loginPage.login(STANDARD_USER.username, STANDARD_USER.password)
+
+    await productsPage.addItem(product);
+
+    await expect(productsPage.shoppingCount).toHaveText('1');
+    });
+}
