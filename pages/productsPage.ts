641c87da628c92b110cd4ca6866ba9999a1c547a
artbereh <a.berehovyi@elsevier.com>
2026-09-18 15:31:13 +0200

    refactor adding products by name

--- pages/productsPage.ts
+++ pages/productsPage.ts
@@ -1,27 +1,23 @@
 import { type Locator, type Page } from '@playwright/test';
 
-export class ProductPage {
+export class ProductsPage {
   readonly page: Page;
-  readonly addButtonBackpack: Locator;
-  readonly addButtonTshirt: Locator;
   readonly shoppingCart: Locator;
   readonly shoppingCount: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
-    this.addButtonBackpack = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
-    this.addButtonTshirt = page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
     this.shoppingCart = page.locator('[data-test="shopping-cart-link"]');
     this.shoppingCount = page.locator('[data-test="shopping-cart-badge"]');
   }
 
-  async addBackpack() {
-    await this.addButtonBackpack.click();
-  }
+  async addItem(itemName: string) {
+    const item = this.page.locator('.inventory_item').filter({
+        hasText: itemName,
+    });
 
-  async addTshirt() {
-    await this.addButtonTshirt.click();
+    await item.getByRole('button', { name: 'Add to cart' }).click();
   }
   
   async openShoppingCart() {
--- tests/UI/product.spec.ts
+++ tests/UI/product.spec.ts
@@ -1,21 +1,21 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/loginPage';
-import { ProductPage } from '../../pages/productPage';
+import { ProductsPage } from '../../pages/productsPage';
 import { CartPage } from '../../pages/cartPage';
 import { WEBSITE, STANDARD_USER } from '../../data/users';
 
 test('should add products to cart', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    const productPage = new ProductPage(page);
+    const productsPage = new ProductsPage(page);
     const cartPage = new CartPage(page);
 
     await loginPage.open(WEBSITE.prod);
     await loginPage.login(STANDARD_USER.username, STANDARD_USER.password)
 
-    await productPage.addBackpack();
-    await productPage.addTshirt();
-    await expect (productPage.shoppingCount).toHaveText('2');
-    await productPage.openShoppingCart();
+    await productsPage.addItem('Sauce Labs Backpack');
+    await productsPage.addItem('Sauce Labs Bolt T-Shirt');
+    await expect (productsPage.shoppingCount).toHaveText('2');
+    await productsPage.openShoppingCart();
 
     await expect(cartPage.backpack).toBeVisible(); 
     await expect(cartPage.tshirt).toBeVisible();
